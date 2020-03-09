ec501556df70b79c717f1e300ba975e74af2877f
frenandi <frenandister@gmail.com>
2020-03-08 21:03:38 -0600

    Removing Shared Credential file and start using environment variables

--- Terraform/CreateTerraformS3Bucket.tf
+++ Terraform/CreateTerraformS3Bucket.tf
@@ -1,8 +1,4 @@
-provider "aws" {
-    region                  = "us-west-2"
-    shared_credentials_file = "C:/Users/Fer/Desktop/CloudTemplatesPractices/Terraform/creds"
-    profile                 = "profile"
-}
+provider "aws" {}
 
 resource "aws_s3_bucket" "test" {
     bucket = "frenandiBucket"
